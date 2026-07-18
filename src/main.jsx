4dd703c96d09d746fb166eef36d64c8667cff58d
patilkhushi418-droid <patilkhushi418@gmail.com>
2026-07-18 22:58:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,36 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
-import { TransactionProvider } from "./context/TransactionContext.jsx";
-
+import { TransactionProvider } from "./context/transactionContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 import "./App.css";
 
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-
-<React.StrictMode>
-
-
-<ThemeProvider>
-
-
-<TransactionProvider>
-
-
-<App />
-
-
-</TransactionProvider>
-
-
-</ThemeProvider>
-
-
-</React.StrictMode>
-
-
+  <React.StrictMode>
+    <ThemeProvider>
+      <TransactionProvider>
+        <App />
+      </TransactionProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 );
