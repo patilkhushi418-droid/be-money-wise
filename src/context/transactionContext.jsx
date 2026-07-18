3fd5155bd9ba0d3f7e63f0dc183c7a84ae95471f
patilkhushi418-droid <patilkhushi418@gmail.com>
2026-07-18 23:44:16 +0530

    Update transactionContext.jsx

--- src/context/transactionContext.jsx
+++ src/context/transactionContext.jsx
@@ -1,186 +1,22 @@
-import { createContext, useEffect, useState } from "react";
-
+import { createContext, useContext, useState } from "react";
 
 export const TransactionContext = createContext();
 
-
-
 export function TransactionProvider({ children }) {
-
-
-  const [transactions, setTransactions] = useState(() => {
-
-    const savedTransactions = localStorage.getItem("transactions");
-
-    return savedTransactions
-      ? JSON.parse(savedTransactions)
-      : [];
-
-  });
-
-
-
-
-
-  // Save data whenever transactions change
-
-  useEffect(() => {
-
-    localStorage.setItem(
-      "transactions",
-      JSON.stringify(transactions)
-    );
-
-  }, [transactions]);
-
-
-
-
-
-
-
-  // Add Transaction
-
-  const addTransaction = (transaction) => {
-
-
-    setTransactions([
-
-      ...transactions,
-
-      {
-        ...transaction,
-        id: Date.now()
-      }
-
-    ]);
-
-
-  };
-
-
-
-
-
-
-
-
-  // Delete Transaction
-
-  const deleteTransaction = (id) => {
-
-
-    const updatedTransactions = transactions.filter(
-
-      (transaction) => transaction.id !== id
-
-    );
-
-
-    setTransactions(updatedTransactions);
-
-
-  };
-
-
-
-
-
-
-
-
-
-  // Calculate Income
-
-  const income = transactions
-
-    .filter(
-
-      (transaction) => transaction.type === "Income"
-
-    )
-
-    .reduce(
-
-      (total, transaction) =>
-        total + Number(transaction.amount),
-
-      0
-
-    );
-
-
-
-
-
-
-
-
-  // Calculate Expense
-
-  const expense = transactions
-
-    .filter(
-
-      (transaction) => transaction.type === "Expense"
-
-    )
-
-    .reduce(
-
-      (total, transaction) =>
-        total + Number(transaction.amount),
-
-      0
-
-    );
-
-
-
-
-
-
-
-
-  // Calculate Balance
-
-  const balance = income - expense;
-
-
-
-
-
-
-
-
+  const [transactions, setTransactions] = useState([]);
 
   return (
-
     <TransactionContext.Provider
-
       value={{
-
         transactions,
-
-        addTransaction,
-
-        deleteTransaction,
-
-        income,
-
-        expense,
-
-        balance
-
+        setTransactions,
       }}
-
     >
-
       {children}
-
     </TransactionContext.Provider>
-
   );
+}
 
-
+export function useTransactions() {
+  return useContext(TransactionContext);
 }
